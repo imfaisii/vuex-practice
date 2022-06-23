b6052f8740e39f8524e2c08bf120ba3cb7e1f4fc
imfaisii <i160289@nu.edu.pk>
2022-06-23 21:32:32 +0500

    added-counter-products

---
+++ resources/js/components/Products/ProductOne.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <li v-for="product in $store.state.products" :key="product.id">
+      Name : {{ product.name }} Price : {{ product.price }}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ resources/js/views/Counter/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center">
+    <div class="counter">
+      <p>{{ $store.state.counter }}</p>
+    </div>
+    <div class="buttons">
+      <button class="btn btn-danger" @click="$store.dispatch('decrement')">
+        -
+      </button>
+      <button class="btn btn-primary" @click="$store.dispatch('increment')">
+        +
+      </button>
+    </div>
+    <div>
+      <router-link class="btn btn-primary mt-2" :to="{ name: 'products' }"
+        >Products</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+div {
+  margin-bottom: 10px;
+}
+.counter {
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
